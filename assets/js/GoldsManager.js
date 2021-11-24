1916a48c8b9a04f627229015014d4ee4ad00a9e9
turpin_l <turpin_l@etna-alternance.net>
2021-11-24 11:16:31 +0100

    minions def

--- assets/js/GoldsManager.js
+++ assets/js/GoldsManager.js
@@ -4,9 +4,16 @@ export function addGold(evt){
     let x = evt.currentTarget.x;
     gameState.golds += x;
 }
+
 export function displayGolds(gameState){
-    let display = document.querySelector("#gold label");
-    display.innerText = gameState.golds;
+    let display = document.querySelector("#gold p");
+
+    display.innerHTML = `Golds : <span>${gameState.golds}</span>`;
+}
+export function displayGps(gameState){
+    let display = document.querySelector("#gps p");
+
+    display.innerHTML = `Gps : <span>${gameState.gps}</span>`;
 }
 export function applyGpsGolds(gameState){
     gameState.golds+=gameState.gps;
