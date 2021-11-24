8864a99b3fc2c1170b0b5ae4df9913ea0a683f4c
turpin_l <turpin_l@etna-alternance.net>
2021-11-24 14:13:39 +0100

    can access to upper variable minion item gen

--- assets/js/MinionShop.js
+++ assets/js/MinionShop.js
@@ -1,11 +1,10 @@
 export function updateShopView(gameState){
     let minions_list = document.getElementById("minion-list");
     minions_list.innerHTML="";
-    console.log(gameState.minions);
     gameState.minions.forEach((item)=>{
         let [elt,info] = createEmptyMinionItem(minions_list);
         updateMinionInfo(info,item);
-        updateMinionCapacities(elt,item.capacities);
+        updateMinionCapacities(elt,item.capacities,item);
     });
 }
 function createEmptyMinionItem(parent){
@@ -17,9 +16,15 @@ function updateMinionInfo(infosParent,minion){
     let title = genElement(infosParent,"h3",minion.name);
     let mem = genElement(infosParent,"p","50Mb");
 }
-function updateMinionCapacities(capParent,capacities){
+function updateMinionCapacities(capParent,capacities,minion){
    capacities.forEach((cap)=>{
        let capItem=genElement(capParent,"div","","minion-capacity");
+       Object.entries(cap).forEach(([key,value])=>{
+           if(value.toString()[0]=="$"){
+               cap[key]=minion[value.slice(1)]
+           }
+       })
+
        let name = genElement(capItem,"p",cap.name);
        let value = genElement(capItem,"p",cap.value);
        let price = genElement(capItem,"p",`${cap.price}$`);
@@ -30,7 +35,6 @@ function updateMinionCapacities(capParent,capacities){
 function genElement(parent,type,inner,classe=""){
     let item = document.createElement(type);
     if(classe!=""){
-
         item.classList.add(classe);
     }
     item.innerHTML=inner;
