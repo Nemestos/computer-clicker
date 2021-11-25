32fbac5beabaa5616e8deb8296f3d3dc6e8942ee
turpin_l <turpin_l@etna-alternance.net>
2021-11-25 11:57:06 +0100

    fix merge

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-import { GameState } from "./State.js";
+import {GameState} from "./State.js";
 import {
   addGold,
   applyGpsGolds,
@@ -12,7 +12,11 @@ import {
 import {handleMultMinions, updateShopView} from "./MinionShop.js";
 import {getExistingsSaves, initListeners, updateExistingSaves} from "./SaveManager.js";
 
+
 var saves = getExistingsSaves();
+console.log(saves);
+console.lo
+var btnAddGold = document.getElementById("clickBtn");
 var state = new GameState();
 btnAddGold.addEventListener("click", addGold);
 btnAddGold.gameState = state;
@@ -23,15 +27,14 @@ updateExistingSaves();
 initListeners(state);
 updateShopView(state);
 
-showClicker(state);
-addEvent(state, addGold);
-
-var handleGpsMinions = setInterval(handleMultMinions, 60, state);
+var handleGpsMinions = setInterval(handleMultMinions, 60, state)
 var gpsMinions = setInterval(getGPS, 1000, state);
 var gpsHandler = setInterval(applyGpsGolds, 1000, state);
 
-var updateTotal = setInterval(displayTotal, 60, state);
-var updateClickPow = setInterval(handleClickPow, 60, state);
+
+var updateTotal = setInterval(displayTotal, 60, state)
+var updateClickPow = setInterval(handleClickPow, 60, state)
 var updateGold = setInterval(displayGolds, 60, state);
 var updateGps = setInterval(displayGps, 60, state);
 var updateClick = setInterval(displayClickValue, 60, state);
+
